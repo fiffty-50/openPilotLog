0c72945a2d47cc4bd52b82209d41bf2186e7dea5
fiffty-50 <felix.turo@gmail.com>
2020-08-26 15:15:52 +0200

    created template for home widget

--- homewidget.cpp
+++ homewidget.cpp
@@ -9,11 +9,18 @@ homeWidget::homeWidget(QWidget *parent) :
 {
     ui->setupUi(this);
     qDebug() << "homeWidget: Activated";
-    ui->backgroundLabel->clear();
 
+    //ui->backgroundLabel->clear();
     //QPixmap pix("://background_gradient.png");
     //ui->backgroundLabel->setPixmap(pix);
-    ui->backgroundLabel->setStyleSheet("background-color: rgba(13, 196, 254, 100);");
+    //ui->backgroundLabel->setStyleSheet("background-color: rgba(13, 196, 254, 20);");
+    /*
+     * To Do: Functions to retreive values from DB
+     */
+
+    ui->totalTimeDisplayLabel->setText("123:45");
+    ui->blockHoursDisplayLabel->setText("123:45");
+    ui->currencyDisplayLabel->setText("17 Take Offs and 15 Landings in the last 90 days.");
 }
 
 homeWidget::~homeWidget()
