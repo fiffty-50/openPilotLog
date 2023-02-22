116a999c1ec0db868e0b8f44eb95514b163b5c62
Felix Turo <felix.turo@gmail.com>
2023-02-22 17:43:36 +0100

    Adding XDG standard directory for flatpak compatibility on Linux

--- src/classes/paths.cpp
+++ src/classes/paths.cpp
@@ -10,6 +10,23 @@ Paths::Paths()
 
 const bool Paths::setup()
 {
+
+// Define the application paths. This will be standard locations on all platforms eventually
+// but for now on Windows and MacOS use the application runtime directory to make it easier to
+// debug and develop. On Linux XDG standard directories are required for the flatpak to work.
+
+#ifdef linux
+    LOG << "Setting up directories at: " << QStandardPaths::writableLocation(QStandardPaths::AppLocalDataLocation);
+    const QString dir_path = QStandardPaths::writableLocation(QStandardPaths::AppLocalDataLocation);
+    for(const auto& str : qAsConst(directories)){
+        QDir dir(dir_path + str);
+        if(!dir.exists()) {
+            if (!dir.mkpath(dir.absolutePath()))
+                return false;
+        }
+    }
+    return true;
+#else
     LOG << "Setting up directories at: " << QCoreApplication::applicationDirPath();
     const QString dir_path = QCoreApplication::applicationDirPath();
     for(const auto& str : qAsConst(directories)){
@@ -20,6 +37,7 @@ const bool Paths::setup()
         }
     }
     return true;
+#endif
 }
 
 const QDir Paths::directory(Directories location)
